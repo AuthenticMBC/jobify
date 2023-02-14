de6875aa753e7aae911172dd942711402665a87c
Maroyi Bisoka <maroyibisoka@Maroyis-MacBook-Air.local>
2023-02-14 20:52:09 +0200

    Auth complete

--- models/User.js
+++ models/User.js
@@ -1,17 +1,18 @@
 import validator from "validator"
 import mongoose from "mongoose"
-
+import bcrypt from "bcryptjs"
+import jwt from "jsonwebtoken"
 const UserSchema = new mongoose.Schema({
     name: {
         type: String,
-        require: [true, 'Please provide name'],
+        required: [true, 'Please provide your name'],
         minlength: 3,
         maxlength: 20,
         trim: true
     },
     email: {
         type: String,
-        require: [true, 'Please provide email'],
+        required: [true, 'Please provide your email'],
         validate: {
             validator: validator.isEmail,
             message: 'Please provide a valid email'
@@ -20,8 +21,9 @@ const UserSchema = new mongoose.Schema({
     },
     password: {
         type: String,
-        require: [true, 'Please provide password'],
-        minlength: 6
+        required: [true, 'Please provide your password'],
+        minlength: 6,
+        select: false
     },
     lastName: {
         type: String,
@@ -38,5 +40,19 @@ const UserSchema = new mongoose.Schema({
         default: 'My City'
     }
 })
+
+UserSchema.pre('save', async function () {
+    const salt = await bcrypt.genSalt(10)
+    this.password = await bcrypt.hash(this.password, salt)
+})
+
+UserSchema.methods.createJWT = function () {
+    return jwt.sign({ userId: this._id }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_LIFETIME })
+}
+
+UserSchema.methods.comparePassword = async function (candidatePassword) {
+    const isMatch = await bcrypt.compare(candidatePassword, this.password)
+    return isMatch
+}
 
 export default mongoose.model('User', UserSchema)
--- server.js
+++ server.js
@@ -6,13 +6,16 @@ dotenv.config()
 import express from 'express'
 
 // express async error
-// import 'express-async-errors'
+import 'express-async-errors'
+
+import morgan from 'morgan'
 
 const app = express()
 
 // middleware
 import notFoundMiddleware from './middleware/not-found.js'
 import errorHandlerMiddleware from './middleware/error-handler.js'
+import authenticateUser from './middleware/auth.js'
 
 // db  and authenticateUser
 import connectDB from './db/connect.js'
@@ -23,14 +26,21 @@ import jobsRouter from './routes/jobsRoutes.js'
 
 const port = process.env.PORT || 8000
 
+if (process.env.NODE_ENV !== 'production') {
+    app.use(morgan('dev'))
+}
+
 app.use(express.json())
 
 app.get('/', (req, res) => {
-    res.send('Welcome!')
+    res.json({ msg: 'Welcome!' })
+})
+app.get('/api/v1', (req, res) => {
+    res.json({ msg: 'API!' })
 })
 
 app.use('/api/v1/auth', authRouter)
-app.use('/api/v1/jobs', jobsRouter)
+app.use('/api/v1/jobs', authenticateUser, jobsRouter)
 
 app.use(notFoundMiddleware)
 app.use(errorHandlerMiddleware)
